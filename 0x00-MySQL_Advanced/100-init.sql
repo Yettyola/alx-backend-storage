8a827d97be195297ca955e71409616ee9669011b
yettyola <yettyolukemi@gmail.com>
2023-07-14 14:01:41 +0000

    task 100

---
+++ 0x00-MySQL_Advanced/100-init.sql
@@ -0,0 +1,11 @@
+-- Initial
+DROP TABLE IF EXISTS corrections;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS projects;
+
+CREATE TABLE IF NOT EXISTS users (
+    id int not null AUTO_INCREMENT,
+    name varchar(255) not null,
+    average_score float default 0,
+    PRIMARY KEY (id)
+);
